a5b85803c707b8131d9817c03ab46c7e30641334
KazumiCha <l.reimann@zooroyal.de>
2018-08-09 14:21:08 +0200

    auskommentieren der auslese.php

--- auslese.php
+++ auslese.php
@@ -1,38 +1,157 @@
+
 <?php
+/* hier wird festgelegt, um was für eine Datei Art es sich handelt in dem Dokument
+in diesem Falle ist es PHP */
 $ordner = "uploads";
-$alledateien = scandir('/uploads'); //Ordner "files" auslesen
-
-foreach ($alledateien as $datei) { // Ausgabeschleife
-    echo $datei."<br />"; //Ausgabe Einzeldatei
-};
-
-
-
-
-
-$verzeichnis = "uploads";
-$uploads_dir = 'uploads';
-foreach ($_FILES["pictures"]["error"] as $key => $error) {
-    if ($error == UPLOAD_ERR_OK) {
-        $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
-        $name = basename($_FILES["pictures"]["name"][$key]);
-        move_uploaded_file($tmp_name, "$uploads_dir/$name");
+/* §ordner ist eine Variable, die beliebig genannt werden kann
+zwischen dem " " kann alles stehen
+es muss nur mindestens ein Symbol enthalten
+es muss mit einem Bustaben oder einem unterstrich beginnen
+es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+es wird zwischen Groß und Kleinschreibung unterschieden  */
+$file = "/uploads";
+/* $file ist eine Variable die beliebig genannt werden kann
+zwischen dem " " kann alles stehen
+es muss nur mindestens ein Symbol enthalten
+es muss mit einem Bustaben oder einem unterstrich beginnen
+es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+es wird zwischen Groß und Kleinschreibung unterschieden  */
+$bilder = array( $file);
+/* $bilder ist eine Variable die beliebig genannt werden kann
+es muss nur mindestens ein Symbol enthalten
+es muss mit einem Bustaben oder einem unterstrich beginnen
+es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+es wird zwischen Groß und Kleinschreibung unterschieden
+array speichert mehrere Werte in einer einzigen Variable
+$file ist eine Variable die beliebig genannt werden kann
+es muss nur mindestens ein Symbol enthalten
+es muss mit einem Bustaben oder einem unterstrich beginnen
+es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+es wird zwischen Groß und Kleinschreibung unterschieden  */
+return $bilder;
+/* return gibt einen Wert zurück
+$bilder ist eine Variable die beliebig genannt werden kann
+es muss nur mindestens ein Symbol enthalten
+es muss mit einem Bustaben oder einem unterstrich beginnen
+es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+es wird zwischen Groß und Kleinschreibung unterschieden */
+echo $bilder;
+/* echo gibt zum Beispiel einen oder mehrere String (ein String stellt eine Kette von Zeichen dar) aus
+$bilder ist eine Variable die beliebig genannt werden kann
+es muss nur mindestens ein Symbol enthalten
+es muss mit einem Bustaben oder einem unterstrich beginnen
+es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+es wird zwischen Groß und Kleinschreibung unterschieden */
+    if (is_dir($verzeichnis)) {
+    /* if führt etwas aus, wenn eine Bedingung wahr ist
+    is_dir diese Funktion überprüft ob die angegebene Datei ein Verzeichnis ist
+    $verzeichnis ist eine Variable die beliebig genannt werden kann
+    es muss nur mindestens ein Symbol enthalten
+    es muss mit einem Bustaben oder einem unterstrich beginnen
+    es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+    es wird zwischen Groß und Kleinschreibung unterschieden
+    { öffnet eine Funktion, um sie zu schließen benötigt man } */
+        if (filetype($file) == "images"
+        /* if führt etwas aus, wenn eine Bedingung wahr ist
+        filetype  gibt den Dateityp einer angegeben Datei oder Verzeichnis wieder
+        $file ist eine Variable die beliebig genannt werden kann
+        zwischen dem " " kann alles stehen
+        es muss nur mindestens ein Symbol enthalten
+        es muss mit einem Bustaben oder einem unterstrich beginnen
+        es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+        es wird zwischen Groß und Kleinschreibung unterschieden
+        images ist ein Dateityp */
+            AND (substr($file, -4) == ".jpg"
+            /* and führt etwas aus, wenn diese und eine andere Bedingung wahr ist
+            substr gibt einen Teil einer Zeichenkette (String) zurück
+            $file ist eine Variable die beliebig genannt werden kann
+            zwischen dem " " kann alles stehen
+            es muss nur mindestens ein Symbol enthalten
+            es muss mit einem Bustaben oder einem unterstrich beginnen
+            es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+            es wird zwischen Groß und Kleinschreibung unterschieden
+            start ist wichtig enorm wichtig um zu wissen wo es am String beginnt
+            die negative Zahl sorgt dafür das es an der angegebenen Position vom der Ende der Zeichenfolge aus gesehen wird
+            == gibt den Wert true aus, wenn beides gleich ist
+            .jpg ist eine Bilddatei Endung */
+                OR substr($file, -4) == ".png"
+                /* or führt etwas aus, wenn diese oder eine andere Bedingung wahr ist
+                substr gibt einen Teil einer Zeichenkette (String) zurück
+                $file ist eine Variable die beliebig genannt werden kann
+                zwischen dem " " kann alles stehen
+                es muss nur mindestens ein Symbol enthalten
+                es muss mit einem Bustaben oder einem unterstrich beginnen
+                es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+                es wird zwischen Groß und Kleinschreibung unterschieden
+                start ist wichtig enorm wichtig um zu wissen wo es am String beginnt
+                die negative Zahl sorgt dafür das es an der angegebenen Position vom der Ende der Zeichenfolge aus gesehen wird
+                == gibt den Wert true aus, wenn beides gleich ist
+                .png ist eine Bilddatei Endung */
+                OR substr($file, -4) == ".gif")
+                /* or führt etwas aus, wenn diese oder eine andere Bedingung wahr ist
+                substr gibt einen Teil einer Zeichenkette (String) zurück
+                $file ist eine Variable die beliebig genannt werden kann
+                zwischen dem " " kann alles stehen
+                es muss nur mindestens ein Symbol enthalten
+                es muss mit einem Bustaben oder einem unterstrich beginnen
+                es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+                es wird zwischen Groß und Kleinschreibung unterschieden
+                start ist wichtig enorm wichtig um zu wissen wo es am String beginnt
+                die negative Zahl sorgt dafür das es an der angegebenen Position vom der Ende der Zeichenfolge aus gesehen wird
+                == gibt den Wert true aus, wenn beides gleich ist
+                .gif ist eine Bilddatei Endung */
+        ) {
+            $filename = "$file";
+            /* $filename ist eine Variable die beliebig genannt werden kann
+            es muss nur mindestens ein Symbol enthalten
+            es muss mit einem Bustaben oder einem unterstrich beginnen
+            es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+            es wird zwischen Groß und Kleinschreibung unterschieden
+            $file ist eine Variable die beliebig genannt werden kann
+            zwischen dem " " kann alles stehen
+            es muss nur mindestens ein Symbol enthalten
+            es muss mit einem Bustaben oder einem unterstrich beginnen
+            es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+            es wird zwischen Groß und Kleinschreibung unterschieden */
+            $destination = "uploads";
+            /* $file ist eine Variable die beliebig genannt werden kann
+            zwischen dem " " kann alles stehen
+            es muss nur mindestens ein Symbol enthalten
+            es muss mit einem Bustaben oder einem unterstrich beginnen
+            es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+            es wird zwischen Groß und Kleinschreibung unterschieden */
+            move_uploaded_file($filename, $destination);
+            /* move upload file verschiebt eine hochgeladene Datei an einem anderen Ort
+            $filename ist eine Variable die beliebig genannt werden kann
+            es muss nur mindestens ein Symbol enthalten
+            es muss mit einem Bustaben oder einem unterstrich beginnen
+            es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+            es wird zwischen Groß und Kleinschreibung unterschieden */
+        }
+        if ($error == UPLOAD_ERR_OK) {
+        /* if führt etwas aus, wenn eine Bedingung wahr ist
+        $error ist eine Variable die beliebig genannt werden kann
+        es muss nur mindestens ein Symbol enthalten
+        es muss mit einem Bustaben oder einem unterstrich beginnen
+        es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+        es wird zwischen Groß und Kleinschreibung unterschieden
+        upload err ok gibt den Wert 0, was bedeutet das die Datei erfolgreich hochgeladen wurde  */
+            move_uploaded_file($tmp_name, "$uploads_dir/$name");
+            /* move upload file verschiebt eine hochgeladene Datei an einem anderen Ort
+            $tmp_name ist eine Variable die beliebig genannt werden kann
+            es muss nur mindestens ein Symbol enthalten
+            es muss mit einem Bustaben oder einem unterstrich beginnen
+            es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+            es wird zwischen Groß und Kleinschreibung unterschieden
+            $uploads_dir ist eine Variable die beliebig genannt werden kann
+            es muss nur mindestens ein Symbol enthalten
+            es muss mit einem Bustaben oder einem unterstrich beginnen
+            es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+            es wird zwischen Groß und Kleinschreibung unterschieden
+            $name ist eine Variable die beliebig genannt werden kann
+            es muss nur mindestens ein Symbol enthalten
+            es muss mit einem Bustaben oder einem unterstrich beginnen
+            es gehen abgesehen von dem Unterstrich keine Sonderzeichen
+            es wird zwischen Groß und Kleinschreibung unterschieden   */
+        }
     }
-}
-
-$file ="uploads";
-if (is_dir($verzeichnis)) {
-    if (filetype($file) == "file"
-        AND (substr($file, -4) == ".jpg"
-        OR substr($file, -4) == ".png"
-        OR substr($file, -4) == ".gif")
-    ) {
-
-        $filename ="";
-        $destination ="";
-        move_uploaded_file($filename , $destination );
-    }
-    if ($error == UPLOAD_ERR_OK) {
-        move_uploaded_file($tmp_name, "$uploads_dir/$name");
-    }
-}
--- index.php
+++ index.php
@@ -1,37 +1,33 @@
+<!DOCTYPE html>
 <!-- hier wird festgelegt, um was für eine Datei Art es sich handelt in dem Dokument,
 bzw wie der Browser es verarbeiten soll. da immer die höchste Datei angebegen wird und php über html steht, wird trotzdem php angegeben -->
-<!DOCTYPE html>
+<html lang="de">
 <!-- html ist ein Tag mit dem gezeigt wird, das hier eine HTML- codierte Seite vorliegt
 lang="de" erklärt den Sprachcode in der die Seite ausgegeben wird (meist landessprache) -->
-<html lang="de">
-<!-- wird zwischen dem html und dem body tag gesetzt
-es wird nicht auf der Webseite angezeigt, außer das title tag
-hier werden die Meta Daten gelagert
-es sorgt für eine Dokumentstruktur
-sie liefern Informationen, steuern die Darstellung und stellen Beziehungen zwischen anderen Datein und Seiten her -->
 <head>
+    <!-- wird zwischen dem html und dem body tag gesetzt
+    es wird nicht auf der Webseite angezeigt, außer das title tag
+    hier werden die Meta Daten gelagert
+    es sorgt für eine Dokumentstruktur
+    sie liefern Informationen, steuern die Darstellung und stellen Beziehungen zwischen anderen Datein und Seiten her -->
+    <title>Bilder hochladen</title>
     <!-- hier wird der angezeigte Name der Website festgelegt
     er wird außerdem als Titelleiste des Tabs, als Standardname beim Lesezeichen (außer man nennt ihn um),
     im Verlauf des Browsers und als Überschrift von Einträgen in Suchmaschinen
     in " " steht der besagte angezeigte Text
     sollte nicht so lang sein (70 höchstens (bei Anzeige bei Google)) -->
-    <title>Bilder hochladen</title>
+    <meta charset="utf-8">
     <!-- meta enthält zusätzliche Informationen von anderen Daten
     sollen das verwalten des Dateiinhalts erleichtern
     setzt sich zumindest aus Attributen zu Eigenschaft und Inhalt zusammen
     charset legt die Zeichenkodierung für die Dateien fest
     utf-8 ist ein Unicode, der besonders für westliche Sprachen genutzt wird. Enthält deutsche Umlaute (ä, ü, ö ect.) -->
-    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <!-- meta enthält zusätzliche Informationen von anderen Daten
     viewport bezeichnet den Anzeigenbereich (für mobile Geräte wichtig)
     content beschreibt Metadaten
     initial-scale gibt den Zoom Wert an, 1.0 werden 1:1 dargestellt
     user-scalable=yes zeigt an ob zoomen möglich ist -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-    <!-- link verweist auf eine Seite oder Datei, bzw verknüpft sich damit
-    href gibt die url der Seite an, auf die verwiesen wird
-    rel wird nur verwendet wenn href vorhanden ist
-    gibt Beziehungen zwischen dem aktuellen und einem anderen Dokument an -->
     <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
     <!-- link verweist auf eine Seite oder Datei, bzw verknüpft sich damit
     href gibt die url der Seite an, auf die verwiesen wird
@@ -39,22 +35,27 @@ sie liefern Informationen, steuern die Darstellung und stellen Beziehungen zwisc
     gibt Beziehungen zwischen dem aktuellen und einem anderen Dokument an -->
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
     <!-- link verweist auf eine Seite oder Datei, bzw verknüpft sich damit
+    href gibt die url der Seite an, auf die verwiesen wird
+    rel wird nur verwendet wenn href vorhanden ist
+    gibt Beziehungen zwischen dem aktuellen und einem anderen Dokument an -->
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    <!-- link verweist auf eine Seite oder Datei, bzw verknüpft sich damit
     rel wird nur verwendet wenn href vorhanden ist
     type bezeichnet den Inhaltstyp
     text wird in einem lesbaren Textzeichenformat angezeigt
     href gibt die url der Seite an, auf die verwiesen wird
     gibt Beziehungen zwischen dem aktuellen und einem anderen Dokument an -->
-    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+</head>
 <!-- wird zwischen dem html und dem body tag gesetzt
 es wird nicht auf der Webseite angezeigt, außer das title tag
 hier werden die Meta Daten gelagert
 sie liefern Informationen, steuern die Darstellung und stellen Beziehungen zwischen anderen Datein und Seiten her
 es sorgt für eine Dokumentenstruktur
 das / beschreibt das Ende und ist zwingend erforderlich (Beispiel /head) -->
-</head>
+<body>
 <!-- beinhaltet den gesamten anzuzeigenden Inhalt
 sorgt für eine Dokumentenstruktur -->
-<body>
+<div id="scrollbereich">
 <!-- schließt mehrere Elemte in einem gemeinsamen Bereich ein
 es sorgt dafür das der Inhalt in einer neuen Zeile erscheint
 div bedeutet division, Abteilung oder Bereich
@@ -63,13 +64,13 @@ gibt eine eindeutige ID, mithilfe CSS und JavaScript verwendet werden kann
 in CSS muss damit die ID ausgewählt wird, ein # vor, Beispiel #scrollbereich
 bei der ID wird zwischen Groß und Kleinschreibung unterschieden
 es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect enthalten -->
-<div id="scrollbereich">
+        <header>
         <!-- beinhaltet den sichtbaren Kopfteil einer Website oder eines Teils einer Seite
         wird für einleitende Inhalte oder Navigationslink verwendet
         kann mehrmals im Dokument genutzt werden
         enthält meistens mehrere Überschriftelemte (h1- h6), Logo oder Symbol und Autoreniformationen
         kann nicht innerhalb eines footer, adresse oder eines anderen header plaziert werden -->
-        <header>
+            <h2><strong>Speicher hier die Bilder deiner Lieblinge</strong></h2>
             <!-- h2 definiert eine Überschrift
             es gibt 6 verschiedene die sich durch die Zahl hinter dem h unterscheiden (Beispiel h2)
             h1 definiert die wichtigste Überschrift
@@ -78,20 +79,21 @@ es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect entha
             es ist ein Phrasen tag
             phrase bedeutet im deutschen sowas wie Satz oder Ausdruck
             mit / wird der tag geschloßen (Beispiel /strong) -->
-            <h2><strong>Speicher hier die Bilder deiner Lieblinge</strong></h2>
+        </header>
         <!-- beinhaltet den sichtbaren Kopfteil einer Website oder eines Teils einer Seite
         wird für einleitende Inhalte oder Navigationslink verwendet
         kann mehrmals im Dokument genutzt werden
         enthält meistens mehrere Überschriftelemte (h1- h6), Logo oder Symbol und Autoreniformationen
         kann nicht innerhalb eines footer, adresse oder eines anderen header plaziert werden
         das / beschreibt das Ende und ist zwingend erforderlich (Beispiel /header) -->
-        </header>
+        <p> Lorem ipsum dolor sit amet, aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Duis autem vel eum  </p>
         <!-- p dient zur Gliederung des Textes
         sorgt für einen Absatz, da Browser Umbrüche in Dokumente ignorieren
         es umfasst einen Textblock, welcher auf der Seite angezeigt wird
+        in diesem Falle ist es ein generierter Text namens Lorem Ipsum
         das / beschreibt das Ende (Beispiel /p) -->
-        <p> Lorem ipsum dolor sit amet, aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            Duis autem vel eum  </p>
+    <img id="rechts" src="https://i.imgur.com/X4KPi3Y.png" alt="Hier soll eigentlich ein Hund sein"/>
     <!-- img verknüfpt ein Bild in einer Html Seite
     es benötigt die Attribute src und alt
     id bedeutet identity
@@ -103,18 +105,6 @@ es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect entha
     wird nur durch eine URL identifiziert
     gibt drei verschiedene Möglichkeiten
     alt gibt den alternativen Text an, wenn das Bild nicht angezeigt werden kann -->
-    <img id="rechts" src="https://i.imgur.com/X4KPi3Y.png" alt="Hier soll eigentlich ein Hund sein"/>
-        <!-- img verknüfpt ein Bild in einer Html Seite
-        es benötigt die Attribute src und alt
-        id bedeutet identity
-        gibt eine eindeutige ID, mithilfe CSS und JavaScript verwendet werden kann
-        in CSS muss damit die ID ausgewählt wird, ein # vor, Beispiel #scrollbereich
-        bei der ID wird zwischen Groß und Kleinschreibung unterschieden
-        es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect enthalten
-        src (englisch source für Quelle)
-        wird nur durch eine URL identifiziert
-        gibt drei verschiedene Möglichkeiten
-        alt gibt den alternativen Text an, wenn das Bild nicht angezeigt werden kann -->
         <img id="links" src="http://ekladata.com/-WzPJzH8EGmgAJKt4VJ4mQp7boI.png" alt="Hier soll eigentlich eine Katze sein">
         <!-- img verknüfpt ein Bild in einer Html Seite
         es benötigt die Attribute src und alt
@@ -128,6 +118,18 @@ es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect entha
         gibt drei verschiedene Möglichkeiten
         alt gibt den alternativen Text an, wenn das Bild nicht angezeigt werden kann -->
         <img id="mitte" src="http://pluspng.com/img-png/hamster-png-hd-mouse-animal-free-png-image-1250.png" alt="Hier soll eigentlich ein Hamster sein">
+        <!-- img verknüfpt ein Bild in einer Html Seite
+        es benötigt die Attribute src und alt
+        id bedeutet identity
+        gibt eine eindeutige ID, mithilfe CSS und JavaScript verwendet werden kann
+        in CSS muss damit die ID ausgewählt wird, ein # vor, Beispiel #scrollbereich
+        bei der ID wird zwischen Groß und Kleinschreibung unterschieden
+        es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect enthalten
+        src (englisch source für Quelle)
+        wird nur durch eine URL identifiziert
+        gibt drei verschiedene Möglichkeiten
+        alt gibt den alternativen Text an, wenn das Bild nicht angezeigt werden kann -->
+        <section id="demo"></section>
         <!-- section bedeutet sowas wie Abschnitt
         sorgt für eine Seitenstruktur
         definiert bestimmte Abschnitte des Dokuemntes
@@ -137,9 +139,9 @@ es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect entha
         bei der ID wird zwischen Groß und Kleinschreibung unterschieden
         es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect enthalten
         das / beschreibt das Ende (Beispiel /section) -->
-        <section id="demo"></section>
+    <form method="get" id="form1">
     <!-- wird verwendet um Benutzereingaben zu sammeln
-    action definiert die Aktion, die beim übermitteln des Formulars ausgeführt werden soll
+    method definiert die Aktion, die beim übermitteln des Formulars ausgeführt werden soll
     get wird benutzt um Daten von einer angegeben Ressource anzufordern
     Anforderungen können zwischengespeichert werden
     Anfragen verbleiben im Browserverlauf
@@ -149,14 +151,17 @@ es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect entha
     Requests werden nur zum Anfordern von Daten verwendet
     id bedeutet identity
     gibt eine eindeutige ID, mithilfe CSS und JavaScript verwendet werden kann
-    in CSS muss damit die ID ausgewählt wird, ein # vor, Beispiel #scrollbereich
+    in CSS muss damit die ID ausgewählt wird, ein # vor, Beispiel #form1
     bei der ID wird zwischen Groß und Kleinschreibung unterschieden
     es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect enthalten-->
-    <form method="get" id="form1">
+            <label>Titel des Bildes:</label>
             <!-- label definiert eine Bezeichnung für input
             wird für den Benutzer als nichts besonderes dargestellt
             das / beschreibt das Ende (Beispiel /label)-->
-            <label>Titel des Bildes:</label>
+        <label>
+        <!-- label definiert eine Bezeichnung für input
+        wird für den Benutzer als nichts besonderes dargestellt -->
+            <input type="text" name="titel" maxlength="35">
             <!-- input gibt ein Eingabefeld an, in das der Benutzer Daten eingeben kann
             wird in form verwendet um Eingabesteuerelemte zu erklären, mit denen Benutzer angaben oder eingeben können
             type bezeichnet den Inhaltstyp
@@ -164,8 +169,6 @@ es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect entha
             name gibt einen Namen für das Element an
             kann verwendet werden um mit Java darauf zu verweisen
             maxlength gibt die maximale Anzahl an Zeichen an, die im input zulässig sind -->
-        <label>
-            <input type="text" name="titel" maxlength="35">
         </label>
         <!-- label definiert eine Bezeichnung für input
         wird für den Benutzer als nichts besonderes dargestellt
@@ -173,8 +176,7 @@ es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect entha
         der Standard Typ ist Text
         file bedeutet zu deutsch Datei
         das / beschreibt das Ende (Beispiel /type)
-        das / beschreibt das Ende (Beispiel /label)
-        type -->
+        das / beschreibt das Ende (Beispiel /label) -->
             <label for="file"><type="file"></type></label>
             <!-- file im Zusammenhang mit input, definiert ein file*select*field und einen Browse Button um Datein hoch zu laden
             type bezeichnet den Inhaltstyp
@@ -183,20 +185,41 @@ es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect entha
             gibt eine eindeutige ID, mithilfe CSS und JavaScript verwendet werden kann
             in CSS muss damit die ID ausgewählt wird, ein # vor, Beispiel #scrollbereich
             bei der ID wird zwischen Groß und Kleinschreibung unterschieden
-            -->
-            <input type="file" id="file">
-
+            es muss mindestens ein Zeichen und darf keine Leerzeichen, Tabulatoren ect enthalten-->
+        <?php include ("auslese.php"); ?>
+        <!-- damit wird dem Dokument mitgeteilt das es in einer anderen Art codieren muss, dadurch muss das ursprüngliche html
+        Dokument mit der Endung .php enden
+        include verweißt auf ein anderes Dokument, was an dieser Stelle angezeigt wird
+        in der Klammer ist die jeweilige Datei, es ist jede Art von Datei möglich -->
+        <form action="auslese.php" method="post" enctype="multipart/form-data"></form>
+        <!-- wird verwendet um Benutzereingaben zu sammeln
+        mit action wird eine Aktion ausgeführt, auslese.php ist die Datei wo die defninerte Aktion protokolliert ist
+        das dokument wird verarbeitet, gelesen und ins html Dokument eingebracht
+         -->
+        <input type="file" id="file">
+        <!-- input gibt ein Eingabefeld an, in das der Benutzer Daten eingeben kann
+        wird in form verwendet um Eingabesteuerelemte zu erklären, mit denen Benutzer angaben oder eingeben können
+        type bezeichnet den Inhaltstyp
+        file definiert den Typ als eine Datei -->
         <button type="submit" form="form1" value="Submit"><a>Bild abschicken</a></button>
-        </form>
-
+        <!-- mit diesem Element werden anklickbare Schaltflächen erzeugt, die durch aktionen ausgelöst werden können.
+        type bezeichnet den Inhaltstyp
+        submit sorgt dafür das etwas gesendet werden soll
+        wird verwendet um Benutzereingaben zu sammeln
+        value (heißt auf deutsch sowas wie Wert) gibt den Anfangswert des Elements an
+        mit a wird ein Abschnitt abgegrenzt welchen man in der css oder php bearbeiten kann, wie in diesem Falle
+        das / beschreibt das Ende (Beispiel /button) -->
+    </form>
+    <!-- wird verwendet um Benutzereingaben zu sammeln
+    das / beschreibt das Ende (Beispiel /form) und schließt damit das Dokument -->
         <p>Es sind nur Formate wie .jpg .gif und .png gültig</p>
-
-    <?php
-    echo "uploads";
-    $file ="uploads";
-    print_r ( $file );
-    include ("auslese.php");
-?>
+        <!-- p sorgen für Textstrukturierungen
+        sind Absätze, da Zeilenumbrüche vom Browser nicht unterstützt werden
+        die Zeichen innerhalb der > < sind der angezeigte Text -->
 </body>
-
+<!-- beinhaltet den gesamten anzuzeigenden Inhalt
+sorgt für eine Dokumentenstruktur
+das / beschreibt das Ende (Beispiel /body) und schließt damit das Dokument -->
 </html>
+<!-- html ist ein Tag mit dem gezeigt wird, das hier eine HTML- codierte Seite vorliegt
+das / beschreibt das Ende (Beispiel /html) und schließt damit das Dokument -->
