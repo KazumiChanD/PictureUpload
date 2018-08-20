7335f85112b02a466378f61b892d119735bb24e1
KazumiCha <l.reimann@zooroyal.de>
2018-08-20 17:36:21 +0200

    Php Datei erstellt, die das logfile.txt ausfüllt, fehlt noch die verknüpfung ins html dokument und die Eintragung von der Datei die nicht verschoben wurde

---
+++ logfile.php
@@ -0,0 +1,15 @@
+<?php
+/* die Serverdaten werden in der Datei logfile.txt gespeichert
+mit der Funktion fopen wird die Datei geöffnet und in den entsprechenden Modus versetzt (a = append (hinzufügen)) */
+$logdatei = fopen("logfile.txt", "a");
+/* in der geöffneten Datei wird mit fputs() eine zeichenfolge geschrieben (Inhalt lies untere Kommentare */
+fputs($logdatei, /* enthält das Datum und die Uhrzeit */
+    date("d.m.Y, H:i:s", time()) . /* mithilfe von "," wird es getrennt und gewährleistet das Programme wie Acces oder Excel es einlesen können
+    mit Request Mode wird dei Art der Anfrage angezeigt */
+    ", " . $_SERVER['REQUEST_METHOD'] . /* mithilfe von "," wird es getrennt und gewährleistet das Programme wie Acces oder Excel es einlesen können
+    mit HTTP User Agent werden Informationen über den Browser und die Seite gesammelt
+    mit \n wird ein Absatz hinzugefügt */
+    ", " . $_SERVER['HTTP_USER_AGENT'] . "\n");
+
+/* schließt die Datei wieder */
+fclose($logdatei);
